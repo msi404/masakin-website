81d7c178641b30c7fc71072b69f68cd717681105
Merced <77894409+ismailMerced@users.noreply.github.com>
2024-01-25 18:53:33 +0300

    fixing typescript

--- components/logos-marquee/logos-marquee.jsx
+++ components/logos-marquee/logos-marquee.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import { cn } from "@/lib/utils";
 import Marquee from "react-marquee-slider";
 import classes from "@/components/logos-marquee/logos-marquee.module.css";
-const LogosMarquee: React.FC = () => {
+const LogosMarquee = () => {
 	const LOGOS = [
 		"https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.newdesignfile.com%2Fpostpic%2F2016%2F05%2Fvector-logo-templates_403097.png&f=1&nofb=1&ipt=08923281a7d5694b97336d198c01e937ac95cfee35432d8f4ddbc2dcf3ee2916&ipo=images",
 		"https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.newdesignfile.com%2Fpostpic%2F2016%2F05%2Fvector-logo-templates_403097.png&f=1&nofb=1&ipt=08923281a7d5694b97336d198c01e937ac95cfee35432d8f4ddbc2dcf3ee2916&ipo=images",
